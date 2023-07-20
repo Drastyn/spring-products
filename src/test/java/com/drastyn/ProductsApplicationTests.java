903219ee8064bd6d061869eba0aabac9d8f11f81
Drastyn <44484749+Drastyn@users.noreply.github.com>
2023-07-19 20:05:37 -0400

    Add - se especifica clase para tests

--- src/test/java/com/drastyn/ProductsApplicationTests.java
+++ src/test/java/com/drastyn/ProductsApplicationTests.java
@@ -3,7 +3,7 @@ package com.drastyn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductsApplicationTests.class)
 class ProductsApplicationTests {
 
 	@Test
